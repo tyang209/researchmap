d2678de7346c334c70afd9da8bc70d9f429f65e5
tyang209 <tyang209@gmail.com>
2015-02-20 16:23:49 -0500

    first commit

---
+++ researchmap/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for researchmap project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'researchmap'
+
+SPIDER_MODULES = ['researchmap.spiders']
+NEWSPIDER_MODULE = 'researchmap.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'researchmap (+http://www.yourdomain.com)'
